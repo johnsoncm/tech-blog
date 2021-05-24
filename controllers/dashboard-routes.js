7be0139d20c60d9f4286352c8f95f509de1fd5b5
Casey Johnson <johnsoncm3@gmail.com>
2021-05-24 13:17:19 -0400

    models are done and dependencies installed

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,6 @@
+
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// 3 get routes
---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Post = require('./Post');
+const { Model } = require('sequelize/types');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Model.exports = { User, Post, Comment};
