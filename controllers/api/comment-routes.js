488e5c58697cadeef52ced23270ba0167a06d403
Casey Johnson <johnsoncm3@gmail.com>
2021-05-31 15:23:03 -0400

    added new.js file

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/' , withAuth, async (req, res) => {
+//add withAuth back at '/' , withAuth, async after testing
+router.post('/' ,  async (req, res) => {
     try {
         const newComment = await Comment.create({
             ...req.body,
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,10 +7,49 @@ const withAuth = require('../utils/auth');
 // 3 get routes
 
 //get all posts associated with the logged-in user
-router.get('/' , withAuth, (req, res) => {
+router.get('/dashboard' , withAuth, (req, res) => {
+    Post.findAll({
+        where:{
+            user_id: req.session.user_id,
+        },
+        attributes: ["id", "title", "body", "user_id"],
+        include: [
+            {
+                model: User,
+                as: "user",
+                attributes: ["name"],
+            },
+            {model: Comment,
+            as: "comments",
+        attributes: ["id", "comment_text", "user_id"],
+    include: [
+        {
+            model: User,
+            as: "user",
+            attributes: ["name"],
+        },
+    ],
+},
+        ],
+    })
 
+})
+.then((dbPostData)=> {
+    if (!dbPostData) {
+        res.status(404).json({message: "No such post available"});
+        return;
+    }
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("dashboard", {posts, loggedIn: req.session.loggedIn});
+})
+.catch((error) => {
+    console.logs(error);
+    res.status(500).json(error);
 })
 
 //get a post to edit
+
+
 
 module.exports = router;
